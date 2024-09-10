c84231632f3d45883cf6cbc9d168e212409e0857
AndreasKuhl <hefbolzen@gmail.com>
2024-09-10 16:53:40 +0200

    create registry

---
+++ Data/registry.dart
@@ -0,0 +1,9 @@
+void registerUser(String username, String password) {
+  if (profile.containsKey(username)) {
+    print('Benutzername bereits vergeben.');
+    return;
+  }
+
+  profile[username] = User(username, password);
+  print('Benutzer erfolgreich registriert.');
+}
