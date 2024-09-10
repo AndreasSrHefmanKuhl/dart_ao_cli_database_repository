2e31307d3000ee436eda854b24d77d8235d7e0fd
AndreasKuhl <hefbolzen@gmail.com>
2024-09-10 10:06:22 +0200

    Add main, and function bot

---
+++ login_user Kopie.dart
@@ -0,0 +1,41 @@
+class User {
+  final String userName;
+  final String password;
+
+  User(this.userName, this.password);
+}
+
+// Simulierte Datenbank zum füllen
+Map<String, User> profile = {};
+
+void registerUser(String username, String password) {
+  if (profile.containsKey(username)) {
+    print('Benutzername bereits vergeben.');
+    return;
+  }
+
+  profile[username] = User(username, password);
+  print('Benutzer erfolgreich registriert.');
+}
+
+bool loginUser(name, password) {
+  User? user = profile[name];
+
+  if (user != null && user.password == password) {
+    print('Anmeldung erfolgreich!');
+    // Hier würde man zum Hauptmenü wechseln (in einer Konsolenanwendung)
+    // ... zum Beispiel:
+    print('Was geht $name! :');
+    return true;
+  } else {
+    print('Ungültige Anmeldedaten.');
+    return false;
+  }
+}
+
+/*void main() {
+  // Beispielhafte Nutzung
+  registerUser('maxmustermann', 'geheim123');
+  loginUser(
+      'maxmustermann', 'geheim121'); // Sollte "Anmeldung erfolgreich!" ausgeben
+}*/
---
+++ main Kopie.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+import 'dart:core';
+import 'bot.dart';
+import 'login_user.dart';
+
+void main() {
+// login User zum Anlegen eines Profils
+//
+//Bot interaktion
+  print("Bitte gib deinen Namen ein und dein Password : ) ");
+  String userInpOne = stdin.readLineSync()!;
+  String userInpTwo = stdin.readLineSync()!;
+
+  registerUser(userInpOne, userInpTwo);
+  loginUser(userInpOne, userInpTwo);
+  bool isProgrammRunning = true;
+
+  while (isProgrammRunning) {
+    print("Was geht? ; ) (B)eenden... (C)hat... (P)rofil ....  ");
+    // warten auf input
+    String userChoiceIn = stdin.readLineSync()!;
+
+    switch (userChoiceIn) {
+      case "b" || "B":
+        isProgrammRunning = false;
+
+      case "c" || "C":
+        // Wird zum Chat mit dem Bot geleitet
+
+        print('Auf gehts zum Bot:.');
+        sleep(Duration(seconds: 1));
+        bot(userChoiceIn); // Botti kan Fragen beantworten, wie es iuhm geht, wie das wetter ist. Fragt ihn doch mal ob er dumm ist ^^
+      case "p" || "P":
+        print("dann wollen wir mal! Auf gehts zu deinem Profil");
+        sleep(Duration(seconds: 1));
+        print("Name : $profile[]");
+    }
+  }
+}
