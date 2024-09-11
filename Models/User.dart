09950f04bcd5cd7a7c0c042c64e1cc011e7a87dd
AndreasKuhl <hefbolzen@gmail.com>
2024-09-11 17:24:37 +0200

    fin abstract

--- Models/User.dart
+++ Models/User.dart
@@ -1,14 +1,14 @@
-import 'Data/login_user.dart';
-import 'Data/registry.dart';
-
-abstract class User {
-  String userName;
+class User {
+  String name;
+  String lastName;
   String password;
+  DateTime birthday;
 
-  User({required this.userName, required this.password});
+  User(
+      {required this.name,
+      required this.lastName,
+      required this.password,
+      required this.birthday});
 }
 
 Map<String, User> profile = {};
-
-Future<void> registry(userName, password) async {}
-void login_user() {}
