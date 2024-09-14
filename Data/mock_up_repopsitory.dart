101f2362644ec15c10dbac69b34245363bb0f465
AndreasKuhl <hefbolzen@gmail.com>
2024-09-14 14:36:27 +0200

    finished list message constructor

--- Data/mock_up_repopsitory.dart
+++ Data/mock_up_repopsitory.dart
@@ -1,10 +1,26 @@
 import '../Models/Messages.dart';
-import '../models/user.dart';
+import '../Models/User.dart';
+import '../Models/bot Kopie.dart';
+
+import '../Data/database_repository.dart';
 
 class MockDatabase implements DatabaseRepository {
   // Der Programmierer soll von außen keinen direkten Zugriff haben.
-  List<User> _users = [User(userName: "kai", password: "passwort")];
-  List<Messages> _messages = [];
+  List<User> _users = [
+    User(
+        name: "Andreas",
+        password: "passwort",
+        lastName: 'Kuhl',
+        birthday: DateTime(87, 30, 10),
+        email: 'hefbolzen@gmail.com')
+  ];
+
+  List<Messages> _messages = [
+    Messages(
+      sender: '',
+      receiver: '',
+    )
+  ];
 
   @override
   List<User> getAllUsers() {
@@ -12,8 +28,19 @@ class MockDatabase implements DatabaseRepository {
   }
 
   @override
-  void addUser(String newUserName, String newPassword) {
-    User newUser = User(userName: newUserName, password: newPassword);
+  // ignore: override_on_non_overriding_member
+  void addUser(
+    String newUserName,
+    String newPassword,
+    DateTime newBirthday,
+    String newLName,
+  ) {
+    User newUser = User(
+        name: newUserName,
+        password: newPassword,
+        lastName: newLName,
+        birthday: newBirthday,
+        email: '');
     _users.add(newUser);
   }
 
@@ -56,5 +83,50 @@ class MockDatabase implements DatabaseRepository {
   Problem getNextProblem() {
     // TODO: implement getNextProblem
     throw UnimplementedError();
+  }
+
+  @override
+  void botChat(Bot bot) {
+    // TODO: implement botChat
+  }
+
+  @override
+  void botFace() {
+    // TODO: implement botFace
+  }
+
+  @override
+  void delUser(User user) {
+    // TODO: implement delUser
+  }
+
+  @override
+  void editMsg(String newContent) {
+    // TODO: implement editMsg
+  }
+
+  @override
+  void logIn(String name, String password) {
+    // TODO: implement logIn
+  }
+
+  @override
+  void recvMsg(Messages message) {
+    // TODO: implement recvMsg
+  }
+
+  @override
+  void registerUser(User user) {
+    // TODO: implement registerUser
+  }
+
+  @override
+  void replyMsg(String replyContent) {
+    // TODO: implement replyMsg
+  }
+
+  @override
+  void sendMsg(Messages message) {
+    // TODO: implement sendMsg
   }
 }
